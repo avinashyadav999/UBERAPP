d793ba9078b60af6767fea08fc46c7615e529b01
avinashyadav999 <aviyaduvanshi12345return@gmail.com>
2026-02-02 19:00:22 +0530

    user registration done

--- Backend/app.js
+++ Backend/app.js
@@ -3,10 +3,17 @@ dotnev.config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const connectToDB = require('./db/db');
+const userRoutes = require('./routes/user.routes');
 
 
+connectToDB();
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+
 
 
 
@@ -14,6 +21,10 @@ app.get('/', (req,res) =>{
     res.send('Hello World');
 
 });
+app.use('/users', userRoutes);
+
+
+
 
 
 module.exports = app;
